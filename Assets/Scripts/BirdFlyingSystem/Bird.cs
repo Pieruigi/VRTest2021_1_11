6e38a7fbe084058966807d650b9f780823cc0646
Pieruigi <pierluigi.zomparelli@gmail.com>
2022-06-14 19:05:02 +0200

    physics

--- Assets/Scripts/BirdFlyingSystem/Bird.cs
+++ Assets/Scripts/BirdFlyingSystem/Bird.cs
@@ -27,15 +27,16 @@ namespace Zomp
         [SerializeField]
         Transform body;
 
-        Vector3 currentVelocity, targetVelocity;
+        Vector3 currentVelocity;//, targetVelocity;
         float currentRollSpeed = 0, targetRollSpeed = 0;
         float rollSmoothTime = 10f;
+        float maxRollSpeed = 30;
         //float rollRecoilSpeed = 10;
 
         float gravityAcceleration = 9.8f;
 
         // 0 means you fall down like a rock, 1 you don't fall at all
-        float wingsResistanceFactor = 0.8f; 
+        float wingsResistanceFactor = 0.008f; 
 
         // 0 means the wing is closed, 1 is open
         // Actual surface is wingMaxSurface * stretchRatio
@@ -43,6 +44,12 @@ namespace Zomp
         float rightWingStretchRatio = 1;
 
         Transform cameraOffset;
+        CharacterController characterController;
+
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
 
         // Start is called before the first frame update
         void Start()
@@ -59,7 +66,7 @@ namespace Zomp
         void Update()
         {
             //ComputeAcceleration();
-            ComputeResistanceAndTorque();
+            
 
             // Recoil from roll
             //Quaternion target = Quaternion.Euler(Vector3.forward * -cameraOffset.transform.localEulerAngles.z);
@@ -68,23 +75,27 @@ namespace Zomp
             // If not grounded add gravity
             if (!IsGrounded())
             {
-                targetVelocity += Vector3.down * gravityAcceleration * Time.deltaTime;
+                currentVelocity += Vector3.down * gravityAcceleration * Time.deltaTime;
             }
-
+            Debug.Log("CurrentVelocity Before:" + currentVelocity.y);
+            ComputeResistanceAndTorque();
+            Debug.Log("CurrentVelocity After:" + currentVelocity.y);
             // Get current velocity
             //currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, velocitySmoothTime * Time.deltaTime);
-            currentVelocity = targetVelocity;
+            //currentVelocity = targetVelocity;
             currentRollSpeed = Mathf.MoveTowards(currentRollSpeed, targetRollSpeed, rollSmoothTime * Time.deltaTime);
             //Debug.Log("TargetVelocity:" + targetVelocity);
             Debug.Log("CurrentVelocity:" + currentVelocity);
             cameraOffset.rotation = Quaternion.AngleAxis(cameraOffset.localEulerAngles.z + currentRollSpeed * Time.deltaTime, cameraOffset.forward);
 
+            //characterController.Move(currentVelocity * Time.deltaTime);
+            //characterController.Move(Vector3.forward * 5 * Time.deltaTime);
         }
 
         void ComputeResistanceAndTorque()
         {
             // Computing resistance and torque depending on the current velocity
-            if (currentVelocity.magnitude > 0)
+            if (true)//currentVelocity.magnitude > 0)
             {
                 Vector3 leftWingRes = Vector3.zero, rightWingRes = Vector3.zero;
                 Vector3 leftTorque = Vector3.zero, rightTorque = Vector3.zero;
@@ -92,7 +103,7 @@ namespace Zomp
                 // Left wing
                 if (leftWingStretchRatio > 0)
                 {
-                    float resFactor = leftWingStretchRatio * wingsResistanceFactor;
+                    float resFactor = leftWingStretchRatio * wingsResistanceFactor / 2f;
                     // Project the velocity on the wing normal to compute the resistance 
                     leftWingRes = Vector3.Project(currentVelocity * resFactor, leftWingPivot.up);
 
@@ -106,7 +117,7 @@ namespace Zomp
                 // Right wing
                 if (rightWingStretchRatio > 0)
                 {
-                    float resFactor = rightWingStretchRatio * wingsResistanceFactor;
+                    float resFactor = rightWingStretchRatio * wingsResistanceFactor / 2f;
                     // Project the velocity on the wing normal to compute the resistance 
                     rightWingRes = Vector3.Project(currentVelocity * resFactor, rightWingPivot.up);
 
@@ -118,7 +129,11 @@ namespace Zomp
                 }
 
                 // Update target velocity
-                targetVelocity += (leftWingRes + rightWingRes) * Time.deltaTime;
+                Vector3 res = (leftWingRes + rightWingRes);
+                if (res.magnitude > currentVelocity.magnitude)
+                    res = res.normalized * currentVelocity.magnitude;
+                    
+                currentVelocity += res;
 
                 // Compute torque
                 leftTorque = Vector3.Project(leftWingRes, body.up);
@@ -126,28 +141,12 @@ namespace Zomp
                 float leftSign = Mathf.Sign(Vector3.Dot(leftTorque, body.up));
                 float rightSign = Mathf.Sign(Vector3.Dot(rightTorque, body.up));
 
-                //cameraOffset.rotation *= Quaternion.AngleAxis(rightTorque.magnitude, Vector3.forward) * Quaternion.AngleAxis(-leftTorque.magnitude, Vector3.forward);
-                //currentRollAcc = (rightTorque.magnitude - leftTorque.magnitude);
-                targetRollSpeed = (rightTorque.magnitude * rightSign - leftTorque.magnitude * leftSign) * 10;
-                targetRollSpeed = Mathf.Clamp(targetRollSpeed, -30, 30);
+                targetRollSpeed = (rightTorque.magnitude * rightSign - leftTorque.magnitude * leftSign) * 100;
+                targetRollSpeed = Mathf.Clamp(targetRollSpeed, -maxRollSpeed, maxRollSpeed);
 
-                // Add roll
-                //cameraOffset.rotation *= Quaternion.AngleAxis(cameraOffset.localEulerAngles.z + currentRollSpeed * Time.deltaTime, cameraOffset.forward);
-                //transform.rotation *= Quaternion.AngleAxis(transform.eulerAngles.z + currentRollSpeed * Time.deltaTime, transform.forward);
-
-                
             }
 
-
-            // Add roll resistance
-            //if (currentRollSpeed != 0)
-            //{
-            //    if (currentRollSpeed > 0)
-            //        currentRollSpeed = Mathf.Max(0, currentRollSpeed - rollResistance * Time.deltaTime);
-            //    else
-            //        currentRollSpeed = Mathf.Min(currentRollSpeed + rollResistance * Time.deltaTime, 0);
-            //}
-            Debug.Log("CurrentRollSpeed:" + currentRollSpeed);
+          
 
         }
 
