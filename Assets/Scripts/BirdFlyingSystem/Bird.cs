9e87848a77cdc58333d48bbb9551021a0465958b
Pieruigi <pierluigi.zomparelli@gmail.com>
2022-06-14 17:42:17 +0200

    Update Bird.cs

--- Assets/Scripts/BirdFlyingSystem/Bird.cs
+++ Assets/Scripts/BirdFlyingSystem/Bird.cs
@@ -1,6 +1,7 @@
 //#define TEST
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 namespace Zomp
@@ -27,17 +28,11 @@ namespace Zomp
         Transform body;
 
         Vector3 currentVelocity, targetVelocity;
-
-        /// <summary>
-        /// This is the whole accelaration given by the sum of all the applied forces 
-        /// </summary>
-        Vector3 acceleration;
-
-        Vector3 torque;
+        float currentRollSpeed = 0, targetRollSpeed = 0;
+        float rollSmoothTime = 10f;
+        //float rollRecoilSpeed = 10;
 
         float gravityAcceleration = 9.8f;
-        float wingMaxSurface = 20;
-        float velocitySmoothTime = 10;
 
         // 0 means you fall down like a rock, 1 you don't fall at all
         float wingsResistanceFactor = 0.8f; 
@@ -47,6 +42,7 @@ namespace Zomp
         float leftWingStretchRatio = 1;
         float rightWingStretchRatio = 1;
 
+        Transform cameraOffset;
 
         // Start is called before the first frame update
         void Start()
@@ -55,7 +51,8 @@ namespace Zomp
             currentVelocity = _testVelocity;
 #endif
 
-           
+            cameraOffset = GetComponent<XROrigin>().CameraFloorOffsetObject.transform;
+            //transform.rotation *= Quaternion.AngleAxis(30, Vector3.forward);
         }
 
         // Update is called once per frame
@@ -63,6 +60,10 @@ namespace Zomp
         {
             //ComputeAcceleration();
             ComputeResistanceAndTorque();
+
+            // Recoil from roll
+            //Quaternion target = Quaternion.Euler(Vector3.forward * -cameraOffset.transform.localEulerAngles.z);
+            //cameraOffset.localRotation = Quaternion.Lerp(cameraOffset.localRotation, target, rollRecoilSpeed * Time.deltaTime);
 
             // If not grounded add gravity
             if (!IsGrounded())
@@ -73,48 +74,80 @@ namespace Zomp
             // Get current velocity
             //currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, velocitySmoothTime * Time.deltaTime);
             currentVelocity = targetVelocity;
+            currentRollSpeed = Mathf.MoveTowards(currentRollSpeed, targetRollSpeed, rollSmoothTime * Time.deltaTime);
             //Debug.Log("TargetVelocity:" + targetVelocity);
             Debug.Log("CurrentVelocity:" + currentVelocity);
+            cameraOffset.rotation = Quaternion.AngleAxis(cameraOffset.localEulerAngles.z + currentRollSpeed * Time.deltaTime, cameraOffset.forward);
+
         }
 
         void ComputeResistanceAndTorque()
         {
-            // You must be in movement
-            if (currentVelocity.magnitude == 0)
-                return;
-
-            Vector3 leftWingRes = Vector3.zero, rightWingRes = Vector3.zero;
-            
-            // Left wing
-            if(leftWingStretchRatio > 0)
+            // Computing resistance and torque depending on the current velocity
+            if (currentVelocity.magnitude > 0)
             {
-                float resFactor = leftWingStretchRatio * wingsResistanceFactor;
-                // Project the velocity on the wing normal to compute the resistance 
-                leftWingRes = Vector3.Project(currentVelocity * resFactor, leftWingPivot.up);
+                Vector3 leftWingRes = Vector3.zero, rightWingRes = Vector3.zero;
+                Vector3 leftTorque = Vector3.zero, rightTorque = Vector3.zero;
 
-                // We are not upside down
-                if (Vector3.Dot(currentVelocity, leftWingRes) > 0)
-                    leftWingRes *= -1;
+                // Left wing
+                if (leftWingStretchRatio > 0)
+                {
+                    float resFactor = leftWingStretchRatio * wingsResistanceFactor;
+                    // Project the velocity on the wing normal to compute the resistance 
+                    leftWingRes = Vector3.Project(currentVelocity * resFactor, leftWingPivot.up);
 
-                Debug.Log("LeftWingRes:" + leftWingRes);
+                    // We are not upside down
+                    if (Vector3.Dot(currentVelocity, leftWingRes) > 0)
+                        leftWingRes *= -1;
+
+                    Debug.Log("LeftWingRes:" + leftWingRes);
+                }
+
+                // Right wing
+                if (rightWingStretchRatio > 0)
+                {
+                    float resFactor = rightWingStretchRatio * wingsResistanceFactor;
+                    // Project the velocity on the wing normal to compute the resistance 
+                    rightWingRes = Vector3.Project(currentVelocity * resFactor, rightWingPivot.up);
+
+                    // We are not upside down
+                    if (Vector3.Dot(currentVelocity, rightWingRes) > 0)
+                        rightWingRes *= -1;
+
+                    Debug.Log("RightWingRes:" + rightWingRes);
+                }
+
+                // Update target velocity
+                targetVelocity += (leftWingRes + rightWingRes) * Time.deltaTime;
+
+                // Compute torque
+                leftTorque = Vector3.Project(leftWingRes, body.up);
+                rightTorque = Vector3.Project(rightWingRes, body.up);
+                float leftSign = Mathf.Sign(Vector3.Dot(leftTorque, body.up));
+                float rightSign = Mathf.Sign(Vector3.Dot(rightTorque, body.up));
+
+                //cameraOffset.rotation *= Quaternion.AngleAxis(rightTorque.magnitude, Vector3.forward) * Quaternion.AngleAxis(-leftTorque.magnitude, Vector3.forward);
+                //currentRollAcc = (rightTorque.magnitude - leftTorque.magnitude);
+                targetRollSpeed = (rightTorque.magnitude * rightSign - leftTorque.magnitude * leftSign) * 10;
+                targetRollSpeed = Mathf.Clamp(targetRollSpeed, -30, 30);
+
+                // Add roll
+                //cameraOffset.rotation *= Quaternion.AngleAxis(cameraOffset.localEulerAngles.z + currentRollSpeed * Time.deltaTime, cameraOffset.forward);
+                //transform.rotation *= Quaternion.AngleAxis(transform.eulerAngles.z + currentRollSpeed * Time.deltaTime, transform.forward);
+
+                
             }
 
-            // Right wing
-            if (rightWingStretchRatio > 0)
-            {
-                float resFactor = rightWingStretchRatio * wingsResistanceFactor;
-                // Project the velocity on the wing normal to compute the resistance 
-                rightWingRes = Vector3.Project(currentVelocity * resFactor, rightWingPivot.up);
 
-                // We are not upside down
-                if (Vector3.Dot(currentVelocity, rightWingRes) > 0)
-                    rightWingRes *= -1;
-
-                Debug.Log("RightWingRes:" + rightWingRes);
-            }
-
-            // Update target velocity
-            targetVelocity += ( leftWingRes + rightWingRes ) * Time.deltaTime;
+            // Add roll resistance
+            //if (currentRollSpeed != 0)
+            //{
+            //    if (currentRollSpeed > 0)
+            //        currentRollSpeed = Mathf.Max(0, currentRollSpeed - rollResistance * Time.deltaTime);
+            //    else
+            //        currentRollSpeed = Mathf.Min(currentRollSpeed + rollResistance * Time.deltaTime, 0);
+            //}
+            Debug.Log("CurrentRollSpeed:" + currentRollSpeed);
 
         }
 
